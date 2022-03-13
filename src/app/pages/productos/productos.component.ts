27f15d393bdf7158d5c077fe8b37fdfcca39b0b9
Rolando HR <rolando167@hotmail.com>
2022-03-13 12:33:05 -0500

    ca

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 import { Producto } from 'src/app/models/Producto';
 import { ProductosService } from 'src/app/services/productos.service';
 
@@ -14,20 +15,29 @@ export class ProductosComponent implements OnInit, OnDestroy {
 
   productos: Producto[] = [];
 
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+  public interObservableSubs$!: Subscription;
+
   constructor(private _productoService: ProductosService) { }
 
   ngOnInit(): void {
     this.obtenerProductos();
   }
   ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    // this._unsubscribeAll.next(null);
+    // this._unsubscribeAll.complete();
 
+    this.interObservableSubs$.unsubscribe();
   }
 
   private obtenerProductos(){
-    this._productoService.getProductos().subscribe(
+    this.interObservableSubs$ = this._productoService.getProductos().subscribe(
       (data)=>{
         console.log(data);
         this.productos = data;
+        this.isLoading = false;
       },
       (error) => {
         console.log('error 🔴');
