ec69a9f2a65a613d7d9a80f50956e6956a9f9df5
Rolando HR <rolando167@hotmail.com>
2022-03-13 10:11:42 -0500

    ca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './pages/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductosComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
