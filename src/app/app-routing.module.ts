a74c7548e322c2a1570cce5921c2927b5ef03f7c
Rolando HR <rolando167@hotmail.com>
2022-03-13 10:37:00 -0500

    ca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'productos', component: HomeComponent },
-  { path: 'usuarios', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'productos', component: ProductosComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
